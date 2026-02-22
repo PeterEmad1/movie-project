425e398e4e4bcc96cfdf41b7c3047624ab2d5865
PeterEmad1 <peteremad132oo5@gmail.com>
2026-02-22 22:14:50 +0200

    3rd commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/movie-project">
       <App />
     </BrowserRouter>
   </StrictMode>,
